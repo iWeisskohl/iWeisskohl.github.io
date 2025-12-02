75de7b7e09f9ed29ec5f81dc6b83a3ec8ee5b3d5
JCW <a1q123456@users.noreply.github.com>
2025-12-02 11:42:27 +0000

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
@@ -9,7 +12,14 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://linghuang.co.uk',
-    integrations: [mdx(), sitemap(), react()],
+    integrations: [mdx({
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    }), sitemap(), react()],
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    },
     vite: {
         server: {
             allowedHosts: ["7f7861e44bb6.ngrok-free.app"],
---
+++ src/content/blog/latex-in-md.md
@@ -0,0 +1,17 @@
+---
+title: 'LaTex In MD'
+description: 'How to use LaTex in markdown files'
+pubDate: 'Dec 02 2025'
+heroImage: '../../assets/blog-placeholder-3.jpg'
+---
+
+
+# Math demo
+
+Inline example: $x^2 + y^2 = z^2$.
+
+Block example:
+
+$$
+f(x) = \frac{1}{\sqrt{2\pi}} e^{-x^2/2}
+$$
